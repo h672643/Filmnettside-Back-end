9113b09b37015574615c47b12019c95a6bb4d2bf
eyup yayan <672643@stud.hvl.no>
2024-10-10 17:30:34 +0200

    Ferdig

---
+++ src/test/java/dev/Eyup/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Eyup.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
